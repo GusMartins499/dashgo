0ebe2bb410f65a000b53d2a36d3b5450aaad8852
Gustavo <gustavosm994@gmail.com>
2022-04-22 09:59:06 -0300

    responsive header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,15 +1,21 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface IProfileProps {
+  showProfileData?: boolean
+}
+
+export function Profile({ showProfileData = true }: IProfileProps) {
   return (
     <Flex align='center'>
-    <Box mr='4' textAlign='right'>
-      <Text>Gustavo Martins</Text>
-      <Text color='gray.300' fontSize='small'>
-        gustavosm994@gmail.com</Text>
-    </Box>
+      {showProfileData && (
+        <Box mr='4' textAlign='right'>
+          <Text>Gustavo Martins</Text>
+          <Text color='gray.300' fontSize='small'>
+            gustavosm994@gmail.com</Text>
+        </Box>
+      )}
 
-    <Avatar size='md' name='Gustavo Martins' src='https://github.com/gusmartins499.png' />
-  </Flex>
+      <Avatar size='md' name='Gustavo Martins' src='https://github.com/gusmartins499.png' />
+    </Flex>
   )
 }
