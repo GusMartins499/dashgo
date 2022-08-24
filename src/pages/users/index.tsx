da14f8e204a81691a430ce5b5cc06d40e500d552
Gustavo <gustavosm994@gmail.com>
2022-08-24 10:11:42 -0300

    stale while revalidate

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -29,7 +29,21 @@ export default function UserList() {
   const { data, isLoading, error } = useQuery('users', async () => {
     const response = await fetch('http://localhost:3000/api/users')
     const data = await response.json()
-    return data
+    const users = data.users.map((user) => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        })
+      }
+    })
+    return users
+  }, {
+    staleTime: 1000 * 60 * 5 // 5 minutos
   })
 
   const isWideVersion = useBreakpointValue({
@@ -80,40 +94,42 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  <Tr>
-                    <Td px={['4', '4', '6']}>
-                      <Checkbox colorScheme='pink' />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight='bold'>Gustavo Martins</Text>
-                        <Text fontSize='small' color='gray.300'>gustavosm994@gmail.com</Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>04 de Abril, 2022</Td>}
-                    <Td>
-                      {isWideVersion ? (
-                        <Button
-                          as='a'
-                          size='sm'
-                          fontSize='small'
-                          colorScheme='purple'
-                          leftIcon={<Icon as={RiPencilLine} fontSize='16' />}>
-                          Editar
-                        </Button>
-                      ) : (
-                        <IconButton
-                          aria-label='Edit user'
-                          icon={<Icon as={RiPencilLine} />}
-                          fontSize='20'
-                          variant='solid'
-                          colorScheme='purple'
-                          size='sm'
-                        >
-                        </IconButton>
-                      )}
-                    </Td>
-                  </Tr>
+                  {data.map((user) => (
+                    <Tr key={user.id}>
+                      <Td px={['4', '4', '6']}>
+                        <Checkbox colorScheme='pink' />
+                      </Td>
+                      <Td>
+                        <Box>
+                          <Text fontWeight='bold'>{user.name}</Text>
+                          <Text fontSize='small' color='gray.300'>{user.email}</Text>
+                        </Box>
+                      </Td>
+                      {isWideVersion && <Td>{user.createdAt}</Td>}
+                      <Td>
+                        {isWideVersion ? (
+                          <Button
+                            as='a'
+                            size='sm'
+                            fontSize='small'
+                            colorScheme='purple'
+                            leftIcon={<Icon as={RiPencilLine} fontSize='16' />}>
+                            Editar
+                          </Button>
+                        ) : (
+                          <IconButton
+                            aria-label='Edit user'
+                            icon={<Icon as={RiPencilLine} />}
+                            fontSize='20'
+                            variant='solid'
+                            colorScheme='purple'
+                            size='sm'
+                          >
+                          </IconButton>
+                        )}
+                      </Td>
+                    </Tr>
+                  ))}
                 </Tbody>
               </Table>
 
